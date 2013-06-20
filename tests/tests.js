78b7d91e6731832316ab5adad27230bc3c47aa28
Mathias Bynens <mathias@qiwi.be>
2013-06-20 12:24:44 +0200

    ucs2decode: Improve support for unpaired surrogates

--- tests/tests.js
+++ tests/tests.js
@@ -180,6 +180,26 @@
 				'description': 'U+DC00 (low surrogate) followed by non-surrogates',
 				'decoded': [56320, 97, 98],
 				'encoded': '\uDC00ab'
+			},
+			{
+				'description': 'High surrogate followed by another high surrogate',
+				'decoded': [0xD800, 0xD800],
+				'encoded': '\uD800\uD800'
+			},
+			{
+				'description': 'Unmatched high surrogate, followed by a surrogate pair, followed by an unmatched high surrogate',
+				'decoded': [0xD800, 0x1D306, 0xD800],
+				'encoded': '\uD800\uD834\uDF06\uD800'
+			},
+			{
+				'description': 'Low surrogate followed by another low surrogate',
+				'decoded': [0xDC00, 0xDC00],
+				'encoded': '\uDC00\uDC00'
+			},
+			{
+				'description': 'Unmatched low surrogate, followed by a surrogate pair, followed by an unmatched low surrogate',
+				'decoded': [0xDC00, 0x1D306, 0xDC00],
+				'encoded': '\uDC00\uD834\uDF06\uDC00'
 			}
 		],
 		'domains': [
