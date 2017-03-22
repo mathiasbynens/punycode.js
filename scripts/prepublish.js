fbd1c801d42044865f08bb8eb4173ea72685176f
Gary Court <gary.court@gmail.com>
2017-03-22 10:04:33 -0600

    Add support for named exports.

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const regex = /module\.exports = punycode;/;
+const output = 'export { ucs2decode, ucs2encode, decode, encode, toASCII, toUnicode };\nexport default punycode;';
 
 const sourceContents = fs.readFileSync(path.resolve(__dirname, '../punycode.js'), 'utf-8');
 
@@ -11,6 +12,6 @@ if (!regex.test(sourceContents)) {
 	throw new Error('The underlying library has changed. Please update the prepublish script.');
 }
 
-const outputContents = sourceContents.replace(regex, 'export default punycode;');
+const outputContents = sourceContents.replace(regex, output);
 
 fs.writeFileSync(path.resolve(__dirname, '../punycode.es6.js'), outputContents);
